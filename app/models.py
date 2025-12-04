cacc0f19a6c1557e0f23aa318badd7ae938d0b66
Kenton Lee <klee0078@uni.sydney.edu.au>
2025-12-04 17:09:01 +1100

    feat(microblog): can run 'flask shell' which automatically imports required contexts and refactored models.py

--- app/models.py
+++ app/models.py
@@ -6,16 +6,20 @@ from app import db
 
 
 '''
-Whenever making changes to the schema use 'flask db migrate -m "<message>" to generate a new migration and then to apply it 'flask db upgrade' (downgrade to rollback).
+Whenever making changes to the schema use 'flask db migrate -m "<message>" to generate a new migration scripts based on changes in the models and then to apply it 'flask db upgrade' (downgrade to rollback).
 '''
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(210), index=True, unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256)) # use of Optional means it is nullable
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                                unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    def __repre__(self):
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+
+    def __repr__(self):
         return '<User {}>'.format(self.username)
 
 class Post(db.Model):
@@ -23,8 +27,9 @@ class Post(db.Model):
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp: so.Mapped[datetime] = so.mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), 
-                                                index = True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                                index=True)
+
     author: so.Mapped[User] = so.relationship(back_populates='posts')
 
     def __repr__(self):
