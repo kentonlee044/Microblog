c91ae068984874583c2d45aa9f7a2456a38797a0
Kenton Lee <klee0078@uni.sydney.edu.au>
2025-12-05 12:40:40 +1100

    feat(login): Allowing users to log in and checking for valid information through the database

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
+from flask_login import LoginManager # Manages user logged in state. Remembers that the user is logged in and provides the 'remember me' functionality
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
 
